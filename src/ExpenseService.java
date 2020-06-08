fc3e8f59cb297efdeb15229a5d9b8cd26cfe5571
Yoni Veksler <vekyoni@icloud.com>
2020-06-08 15:52:25 +0300

    changed expenses from list to hashmap with categoryId as a key and list of expenses of that category

--- src/ExpenseService.java
+++ src/ExpenseService.java
@@ -4,6 +4,7 @@ import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import org.apache.commons.io.IOUtils;
@@ -11,16 +12,20 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 public class ExpenseService {
-	List<Expense> expenses;
-	private static final String restURL="http://schoolproject-env.eba-qp6e4y82.us-east-2.elasticbeanstalk.com/" + "api/expense";
-	// PRODUCTION: private static final String restURL = "http://localhost:3000/api/expense";
+	// List<Expense> expenses;
+	HashMap<String, List<Expense>> expenses;
+	private static final String restURL = "http://schoolproject-env.eba-qp6e4y82.us-east-2.elasticbeanstalk.com/"
+			+ "api/expense";
+	// PRODUCTION: private static final String restURL =
+	// "http://localhost:3000/api/expense";
 	// Injection of services:
 	private final CategoryService categoryService = CategoryService.getInstance();
 	private final UserService userService = UserService.getInstance();
 
 	// Singleton-
 	private ExpenseService() {
-		expenses = new ArrayList<>();
+		// expenses = new ArrayList<>();
+		expenses = new HashMap<>();
 	}
 
 	private static ExpenseService INSTANCE = new ExpenseService();
@@ -31,27 +36,26 @@ public class ExpenseService {
 	// ---------
 
 	// returns an array of expenses that belong to a certain category of choice
-	public Expense[] findExpensesByCategory(String title) {
-		String categoryId = categoryService.findCategory(title).getId();
-		ArrayList<Expense> foundExpenses = new ArrayList<>();
-		for (Expense exp : expenses) {
-			if (exp.getId().contentEquals(categoryId)) {
-				foundExpenses.add(exp);
-			}
-		}
-
-		return (Expense[]) foundExpenses.toArray();
-
+	public Expense[] findExpensesByCategory(String categoryName) {
+		return (Expense[]) this.expenses.get(categoryService.findCategory(categoryName).getId()).toArray();
 	}
+
 	public void printExpensesByCategory() {
-		for(Category cat:categoryService.categories) {
-			System.out.println(cat);
-			for(Expense exp:this.expenses) {
-				if(exp.getCategoryId().equals(cat._id)) {
-					System.out.println(exp);
-				}
+//		for(Category cat:categoryService.categories) {
+//			System.out.println(cat);
+//			for(Expense exp:this.expenses) {
+//				if(exp.getCategoryId().equals(cat._id)) {
+//					System.out.println(exp);
+//				}
+//			}
+//		}
+		this.expenses.forEach((categoryId, expenseList) -> {
+			System.out.println("CategoryId: "+categoryId);
+			for (Expense exp : expenseList) {
+				System.out.println(exp);
 			}
-		}
+			System.out.println("---------");
+		});
 	}
 
 	// Adding an expense and updating the amountUsed in that category!
@@ -87,7 +91,15 @@ public class ExpenseService {
 			JSONObject myResponse = new JSONObject(response);
 			String _id = myResponse.getString("_id");
 			Expense expense = new Expense(title, amount, userService.getUserId(), _id, foundCategory.getId());
-			this.expenses.add(expense);
+			// this.expenses.add(expense);
+			if(this.expenses.containsKey(foundCategory.getId())) {
+				this.expenses.get(foundCategory.getId()).add(expense);
+			}
+			else {
+				List<Expense> newList=new ArrayList<>();
+				newList.add(expense);
+				this.expenses.put(foundCategory.getId(),newList );
+			}
 			foundCategory.amountUsed += amount;
 			this.categoryService.updateCategory(foundCategory, userService.getToken());
 			// Closing connection
@@ -126,9 +138,18 @@ public class ExpenseService {
 			this.expenses.clear();
 			for (int i = 0; i < size; i++) {
 				JSONObject obj = expJSONS.getJSONObject(i);
+				String categoryId=obj.getString("categoryId");
 				Expense expense = new Expense(obj.getString("title"), obj.getInt("amount"), obj.getString("creator"),
-						obj.getString("_id"), obj.getString("categoryId"));
-				this.expenses.add(expense);
+						obj.getString("_id"), obj.getString(categoryId));
+				//this.expenses.add(expense);
+				if(this.expenses.containsKey(obj.getString(categoryId))) {
+					this.expenses.get(categoryId).add(expense);
+				}
+				else {
+					List<Expense> newList=new ArrayList<>();
+					newList.add(expense);
+					this.expenses.put(categoryId,newList );
+				}
 			}
 			// Closing connection
 			in.close();
@@ -147,77 +168,78 @@ public class ExpenseService {
 		return true;
 	}
 
-	
-	//Returns foundExpense or null if not exists!
+	// Returns foundExpense or null if not exists!
 	public Expense findExpense(String title, int amount, String categoryId) {
-		for (Expense exp : this.expenses) {
-			if (exp.title.equalsIgnoreCase(title) && exp.amount == amount
-					&& exp.getCategoryId().equalsIgnoreCase(categoryId)) {
-              return exp;
+		for(Expense exp:this.expenses.get(categoryId)) {
+			if(exp.title.equalsIgnoreCase(title)&&exp.amount==amount) {
+				return exp;
 			}
 		}
 		return null;
-}
+	}
 
 	// Deleting a certain expense!
-	public boolean deleteExpense(String title, int amount,String categoryTitle) {
-		 Category foundCategory=categoryService.findCategory(categoryTitle);
-		 if(foundCategory==null) {return false;}
-		 Expense foundExpense=findExpense(title,amount,foundCategory._id);
-		 String query_url = restURL+"/"+foundExpense.getId();
-         boolean result = true;
-         try {
-             URL url = new URL(query_url);
-             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-             conn.setConnectTimeout(5000);
-             //Setting header: for authorization
-             conn.setRequestProperty("Authorization", "Bearer " + userService.getToken());
-             conn.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
-             conn.setDoOutput(true);
-             conn.setDoInput(true);
-             conn.setRequestMethod("DELETE");
-             // read the response
-             this.expenses.removeIf(exp->{
-            	 return exp.getId().equals(foundCategory._id);
-             });
-             foundCategory.amountUsed-=amount;
-             categoryService.updateCategory(foundCategory, userService.getToken());
+	public boolean deleteExpense(String title, int amount, String categoryTitle) {
+		Category foundCategory = categoryService.findCategory(categoryTitle);
+		if (foundCategory == null) {
+			return false;
+		}
+		Expense foundExpense = findExpense(title, amount, foundCategory._id);
+		String query_url = restURL + "/" + foundExpense.getId();
+		boolean result = true;
+		try {
+			URL url = new URL(query_url);
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setConnectTimeout(5000);
+			// Setting header: for authorization
+			conn.setRequestProperty("Authorization", "Bearer " + userService.getToken());
+			conn.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+			conn.setDoOutput(true);
+			conn.setDoInput(true);
+			conn.setRequestMethod("DELETE");
+			// read the response
+			this.expenses.get(foundCategory.getId()).remove(foundExpense);
+			foundCategory.amountUsed -= amount;
+			categoryService.updateCategory(foundCategory, userService.getToken());
 
-             //Closing connection
-             conn.disconnect();
-         } catch (Exception e) {
-             //In case of unsuccessful response with status of other then 200/201....
-             System.out.println("Error");
-             result = false;
-         }
-         return result;
-    }
-	
+			// Closing connection
+			conn.disconnect();
+		} catch (Exception e) {
+			// In case of unsuccessful response with status of other then 200/201....
+			System.out.println("Error");
+			result = false;
+		}
+		return result;
+	}
+
 	public boolean deleteAllExpensesInCategory(String categoryTitle) {
-		String categoryId=categoryService.findCategory(categoryTitle).getId();
-		 String query_url = restURL+"/"+categoryId;
-         boolean result = true;
-         try {
-             URL url = new URL(query_url);
-             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-             conn.setConnectTimeout(5000);
-             //Setting header: for authorization
-             conn.setRequestProperty("Authorization", "Bearer " + userService.getToken());
-             conn.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
-             conn.setDoOutput(true);
-             conn.setDoInput(true);
-             conn.setRequestMethod("DELETE");
-             //---------------
-             //TODO?
-             //Update the category amountUsed to 0 ?
-             //Closing connection
-             conn.disconnect();
-         } catch (Exception e) {
-             //In case of unsuccessful response with status of other then 200/201....
-             System.out.println("Error");
-             result = false;
-         }
-         return result;
+		Category foundCategory=categoryService.findCategory(categoryTitle);
+		String categoryId = foundCategory.getId();
+		String query_url = restURL + "/" + categoryId;
+		boolean result = true;
+		try {
+			URL url = new URL(query_url);
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setConnectTimeout(5000);
+			// Setting header: for authorization
+			conn.setRequestProperty("Authorization", "Bearer " + userService.getToken());
+			conn.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+			conn.setDoOutput(true);
+			conn.setDoInput(true);
+			conn.setRequestMethod("DELETE");
+			// ---------------
+			this.expenses.remove(categoryId);
+			foundCategory.amountUsed=0;
+			categoryService.updateCategory(foundCategory,userService.getToken());
+			
+			// Closing connection
+			conn.disconnect();
+		} catch (Exception e) {
+			// In case of unsuccessful response with status of other then 200/201....
+			System.out.println("Error");
+			result = false;
+		}
+		return result;
 	}
 
 }
